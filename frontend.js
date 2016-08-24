2bb63396b30e8600bac2facc29f4164ba2a350d4
Matt Chan <wildcatrobotics2011@gmail.com>
2016-08-23 17:58:01 -0700

    finished background resize and moving

--- frontend.js
+++ frontend.js
@@ -1,10 +1,29 @@
 (function() {
 
-    var width, height, image = document.getElementById('image-background');
+
+    var image = document.getElementById('image-background');
+
     var imagestart = {left: window.getComputedStyle(image).getPropertyValue('left') , top: window.getComputedStyle(image).getPropertyValue('top')};
     // Main
     resize();
+
+    var width = image.width;
+    var height = image.height;
     addListeners();
+
+    function resize() {
+        var minheight = window.innerheight*1.2;
+        var minwidth = window.innerwidth*1.2;
+        var windowscale = minheight/minwidth;
+        var imagescale = image.naturalHeight/image.naturalWidth;
+        if(windowscale>image.scale){    //window height is bigger, align factor
+            image.height = minheight;
+            image.width = 'auto';
+        }else{
+            image.width = minwidth;
+            image.height = 'auto';
+        }
+    }
 
     // Event handling
     function addListeners() {
@@ -15,9 +34,6 @@
     }
 
     function mouseMove(e) {
-        //console.log('mouse is moving');
-        //console.log(image.style.left,image.style.top,image.style.right,image.style.bottom);
-        console.log();
         var posx = posy = 0;
         if (e.pageX || e.pageY) {
             posx = e.pageX;
@@ -27,13 +43,8 @@
             posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
-        image.style.left = parseInt(imagestart.left, 10)+(width/2-posx)/30 +'px';
-        image.style.top = parseInt(imagestart.top, 10)+(height/2-posy)/30 + 'px';
+        image.style.left = parseInt(imagestart.left, 10)-(width/2-posx)/100 +'px';
+        image.style.top = parseInt(imagestart.top, 10)-(height/2-posy)/100 + 'px';
         }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-    }
 
 })();
