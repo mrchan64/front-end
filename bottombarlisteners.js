484377e9e139240ffe1dd1d74ee41bb8476fde9d
Matt Chan <wildcatrobotics2011@gmail.com>
2016-08-24 21:44:12 -0700

    temp working bot bar

--- bottombarlisteners.js
+++ bottombarlisteners.js
@@ -8,33 +8,55 @@
 
     function initstuff(){
         menuItems.each(function(){
-            var circ = $(this).find('.expanding-circle')[0]
+            var circbox = $(this).find('.expanding-circle-box')[0]
             var tab = $(this).children()[0]
-            var extra = $(circ).height();
-            var current = $(circ).position().top
-            var height = $(tab).height();
-            $(tab).on('mouseover', circleOnDelegate(tab,circ,current,height,extra));
+            $(tab).on('mouseenter', function(){
+                $(circbox).stop()
+                var high = $(circbox).data("high");
+                $(circbox).animate({top: high}, 200);
+            });
+            $(tab).on('mouseleave', function(){
+                $(circbox).stop()
+                var low = $(circbox).data("low");
+                $(circbox).animate({top: low}, 200);
+            });/*
+            $(circ).on('mouseenter', function(){
+                $
+                $(circ).animate({top: high}, 200);
+            });
+            $(circ).on('mouseleave', function(){
+                console.log(2);
+                $(circ).stop()
+                var low = $(circ).data("low");
+                $(circ).animate({top: low}, 200);
+            });*/
         });
     }
 
-    function scrollDelegate(e){
+    var scrollDelegate = function(e){
 
-    }
-
-    function circleOnDelegate(tab, circ, current,height,extra){
-        console.log(1);
-        $(tab).off('mouseover');
-        $(circ).animate({top: current-(height*10+extra)}, '.2s');
     }
 
     function resize(){
         menuItems.each(function(){
             var width = $(this).innerWidth();
             var height = $(this).innerHeight();
-            $(this).find('.expanding-circle').each(function(){
+            $(this).find('.expanding-circle-box').each(function(){
                 $(this).width(width/2).height(width/2);
                 $(this).css({top: -width/8, left: width/4, position: 'absolute'})
             });
+            var circbox = $(this).find('.expanding-circle-box')[0]
+            var circ = $(this).find('.expanding-circle')[0]
+            $(circ).css({width: '100%', height: '100%'});
+            var tab = $(this).children()[0];
+            var extra = $(circbox).height();
+            var current = $(circbox).position().top;
+            var height = $(tab).height();
+            $(circbox).data('high', current-(height*10+extra));
+            $(circbox).data('low', current);
+            var wind = $(window);
+            var maxheight = $(wind).innerHeight() * .75;
+            $(tab).data('max', maxheight);
         });
     }
 
---
+++ filewindow.js
@@ -0,0 +1,17 @@
+(function() {
+
+	var activeWindows = $('.window-placeable');
+
+	var newwindow = function(){
+
+	}
+
+	function newwinhtml(){
+		
+	}
+
+	function update() {
+		activeWindows = $('.window-placeable');
+	}
+
+})();
