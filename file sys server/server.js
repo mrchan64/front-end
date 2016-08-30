41d315fc7ffc1c696ba261bd177ef42d75ef910c
Matt Chan <wildcatrobotics2011@gmail.com>
2016-08-30 16:19:35 -0700

    moved stuff around, need to fix get

--- file sys server/server.js
+++ file sys server/server.js
@@ -1,10 +1,20 @@
 var express = require("express");
+var cors = require("cors");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/children', cors(), function (req, res) {
+	var numberVars = Math.floor(Math.random()*12)+1;
+	var lul = [];
+	for(var i = 0; i < numberVars; i++){
+		var rand = JSON.stringify(Math.floor(Math.random()*10));
+		lul.push(rand);
+	}
+	var retSon = {
+		'hi': lul
+	}
+	res.json(retSon);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Server started');
 });
