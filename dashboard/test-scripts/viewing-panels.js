dca3037fdaffdeb4be0f92152cb4807a47fa99cc
mrchan <wildcatrobotics2011@gmail.com>
2017-01-09 10:46:01 -0800

    finished viewing panels

--- dashboard/test-scripts/viewing-panels.js
+++ dashboard/test-scripts/viewing-panels.js
@@ -9,14 +9,90 @@ var panelManager = new PanelManager(dashboard);
 
 function Panel(obj, height, posy) {
 	this.element = obj;
-	this.width = width/80;
-	this.height = height;
+	this.w = width/80;
+	this.width = this.w;
+	this.h = height;
+	this.height = this.h;
 	this.x = width;
-	this.y = posy
+	this.posy = posy;
+	this.y = this.posy
 	this.element.css({height: this.height, top: this.y, width: this.width});
 	var radius = height/10;
 	this.element.css("border-top-left-radius", radius);
 	this.element.css("border-bottom-left-radius", radius);
+	var e = this;
+
+	this.checkOver = function(mouseX, mouseY){
+		if(e.x-e.width<=mouseX && e.y<=mouseY && mouseY<=e.y+e.height){
+			if(mouseX<e.x-e.width+radius && mouseY<e.y+radius){
+				if(distance(mouseX, mouseY, e.x-e.width+radius, e.y+radius)<=radius){
+					return true;
+				}else{
+					return false;
+				}
+			}
+			if(mouseX<e.x-e.width+radius && mouseY>e.y+e.height-radius){
+				if(distance(mouseX, mouseY, e.x-e.width+radius, e.y+e.height-radius)<=radius){
+					return true;
+				}else{
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	this.hov = false;
+	this.cli = false;
+	this.shi = false;
+
+	this.hovered = function(){
+		if(this.cli)return;
+		this.hov = true;
+		this.width = this.w*8;
+		this.element.stop();
+		this.element.animate({width: this.width}, 100);
+	}
+
+	this.unhovered = function(){
+		if(this.cli)return;
+		this.hov = false;
+		this.width = this.w;
+		this.element.stop();
+		this.element.animate({width: this.width}, 100);
+	}
+
+	this.clicked = function(){
+		this.hov = false;
+		this.cli = true;
+		this.shi = false;
+		this.width = this.w*30;
+		this.height = this.h*ymultiplier;
+		this.y = this.posy;
+		this.element.stop();
+		this.element.animate({width: this.width, height: this.height, top: this.y}, 100);
+	}
+
+	this.unclicked = function(){
+		this.cli = false;
+		this.shi = false;
+		this.width = this.w;
+		this.height = this.h;
+		this.y = this.posy;
+		this.element.stop();
+		this.element.animate({width: this.width, height: this.height, top: this.y},100);
+	}
+
+	this.shifted = function(){
+		this.cli = false;
+		this.shi = true;
+		this.width = this.w;
+		this.height = this.h;
+		this.y = this.posy + this.h*(ymultiplier-1);
+		this.element.stop();
+		this.element.animate({width: this.width, height: this.height, top: this.y},100);
+	}
 }
 
 function PanelManager(obj) {
@@ -33,14 +109,41 @@ function PanelManager(obj) {
 		var p = $("<div></div>");
 		p.addClass("panel-base");
 		var panel = new Panel(p, height, posYindex);
-		this.panels.unshift(panel);
+		this.panels[this.panels.length]=panel;
 		this.element.append(panel.element);
 		posYindex += height;
 	}
 
-	this.onHover = function(event){
+	this.onHov = function(event){
 		for(i = 0; i<e.panels.length; i++){
-			if()
+			if(e.panels[i].checkOver(event.clientX,event.clientY)){
+				if(!e.panels[i].hov)e.panels[i].hovered();
+			}else{
+				if(e.panels[i].hov)e.panels[i].unhovered();
+			}
 		}
 	}
+
+	this.onCli = function(event){
+		var shifting = false;
+		for(i = 0; i<e.panels.length; i++){
+			if(shifting){
+				if(!e.panels[i].shi)e.panels[i].shifted();
+				continue;
+			}
+			if(e.panels[i].checkOver(event.clientX,event.clientY)){
+				if(!e.panels[i].cli)e.panels[i].clicked();
+				shifting = true;
+			}else{
+				if(e.panels[i].shi || e.panels[i].cli)e.panels[i].unclicked();
+			}
+		}
+	}
+
+	$(window).on("mousemove", this.onHov);
+	$(window).on("mousedown", this.onCli);
+}
+
+function distance(x1, y1, x2, y2){
+	return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
 }
